5f6c92a290f2b6c6700c38e35246b30d54770753
2Art-Sm3 <artem_smirnov_smirnov@gmail.com>
2026-03-05 21:55:54 +0500

    Первые наработки

---
+++ src/main/java/ru/artem/user_service/entity/User.java
@@ -0,0 +1,30 @@
+package ru.artem.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
